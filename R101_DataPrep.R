501e3f1b7fa946c88c39b68428aab86cfc554f27
Song <xsm7f@umsystem.edu>
2021-10-23 06:18:40 -0500

    update DataPrep script

--- R101_DataPrep.R
+++ R101_DataPrep.R
@@ -3,24 +3,23 @@
 #######################################################################################
 
 pacman::p_load(tidyverse)
-
-bmi_cohort<-readRDS()
+aki_cohort<-readRDS("D:/NextGenBMI-MUIRB2074022/data/aki_cohort.rda")
 
 #====================================================================================
 # Inspect your data
 #====================================================================================
 
 #Use head() to view the first few rows.
-head(surveys)
+head(aki_cohort)
 
 #Use tail() to view the first few rows.
-tail(surveys)
+tail(aki_cohort)
 
 #Use str() to look at the structure of the data
-str(surveys)
+str(aki_cohort)
 
 #Use summary() to get a quick overview of the data
-summary()
+summary(aki_cohort)
 
 #Use the Metadata table to understand meaning of each column in each table
 metadata<-read.csv("D:/NextGenBMI-MUIRB2074022/data/metadata.csv")
